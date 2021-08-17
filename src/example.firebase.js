caa9152d017e173b59221b712ed4f77118a166cb
lenin <lnnsntg@gmail.com>
2021-08-17 16:52:50 +0200

    Primer commit añadi firebase

---
+++ src/example.firebase.js
@@ -0,0 +1,21 @@
+/* import fireApp from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+// Initialize Firebase
+fireApp.initializeApp(firebaseConfig);
+
+fireApp.analytics();
+
+const db = fireApp.firestore();
+
+const auth = fireApp.auth(); */
